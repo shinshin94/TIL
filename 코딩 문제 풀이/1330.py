4ac8ba68d844b0f68ec17c6dd7f715cd15a61cea
태가놈 <aodvl2@gmail.com>
2022-01-12 20:44:35 +0900

    오늘의 문제 1

---
+++ 코딩 문제 풀이/1330.py
@@ -0,0 +1,8 @@
+#둘중에 큰것 구하기
+A,B = map(int,input().split())
+if A>B:
+    print(">")
+elif A<B:
+    print("<")
+else:
+    print("==")
---
+++ 코딩 문제 풀이/9498.py
@@ -0,0 +1,12 @@
+#숫자를 입력받고 학점 계산
+X = int(input())
+if X >=90:
+    print("A")
+elif X>=80:
+    print("B")
+elif X >=70:
+    print("C")
+elif X >= 60:
+    print("D")
+else:
+    print("F")
